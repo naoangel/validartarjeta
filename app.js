5218fc932078b26aff0f778f9293a722231c30c4
naoangel <es.veneros@gmail.com>
2017-10-05 10:58:17 -0300

    programa para validar tarjeta

---
+++ app.js
@@ -0,0 +1,15 @@
+
+ var numTarj = prompt("Ingrese su número de tarjeta")
+
+   if(numTarj!= "" && ) { // si el número de tarjeta es diferente de vacío 
+      return isAValid();
+  }
+
+var  function isAValidCard(){
+   
+for (var i = 0; i < numTarj.length; i++) {
+  var item = numTarj.pop();    
+  numTarj.splice(i, 0, item);
+}  // este for es para dar vuelta los números de la tarjeta
+
+}
